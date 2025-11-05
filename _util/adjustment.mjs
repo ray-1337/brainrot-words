5ca0e707e19930b085f49ce96aa15d5e5118e949
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-11-05 16:48:47 +0800

    Whitelist dash symbol/char in word

--- _util/adjustment.mjs
+++ _util/adjustment.mjs
@@ -24,7 +24,7 @@ async function init() {
       .filter(word => (word.split(/\s/).length > maxWordsLength || word.length > maxCharsLength) !== true)
 
     const formattedArray = Array.from(new Set(
-      array.map(word => word.trim().toLowerCase().replace(/[^\w\d\s\+'\/]/gim, ""))
+      array.map(word => word.trim().toLowerCase().replace(/[^\w\d\s\+'\/\-]/gim, ""))
     ));
     
     await writeFile(_path, formattedArray.join("\n"), { encoding });
